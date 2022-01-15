de9196d743db5159d172ca85a7430ef8871cd0a8
manumos <manuela.moscoso@est.ikiam.edu.ec>
2022-01-15 11:46:46 -0500

    Moscoso Manuela ha resuelto el ejercicio 8

---
+++ ejercicio8.py
@@ -0,0 +1,11 @@
+lista1= ["Mazda RX4", "Mazda RX4 Wag", "Datsun 710", "Hornet 4 Drive", "Hornet Sportabout", "Valiant", "Duster 360", "Merc 240D", "Merc 230"]
+print (lista1)
+lista1[3]
+lista1.append(["gbi6", "bio1", "ikiam"])
+
+lista1[-1][-3]
+lista1[2:5]
+lista1.append(["gbi6", "bio1", "ikiam"])
+
+len(lista1)
+lista1[4]
