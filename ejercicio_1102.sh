4a1fdc3271a7a7a5b65d4473c497889cd4631d93
manumos <manuela.moscoso@est.ikiam.edu.ec>
2022-01-15 12:45:17 -0500

    Actualizacion notebook

--- ejercicio_1102.sh
+++ ejercicio_1102.sh
@@ -2,6 +2,19 @@
 # $1 = nombre del archivo
 # $2 =numero del individuo
 
+#1
+echo "Muestras para ID 3"
+cut -f 1 $1 | grep -c -w 3
+
+echo "Muestras para el ID 4"
+cut -f 1 $1 | grep -c -w 27
+
+
+
+
+
+
+
 cut -f 1 $1 >1.csv
 tail -n +2 1.csv >2.csv
 uniq -c 2.csv
