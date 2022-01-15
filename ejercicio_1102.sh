93bb4c4ba61af004fe3569d6ea3e00287ca67cfe
manumos <manuela.moscoso@est.ikiam.edu.ec>
2022-01-15 13:12:30 -0500

    Moscoso Manuela ha resuelto el ejercicio 4

--- ejercicio_1102.sh
+++ ejercicio_1102.sh
@@ -1,24 +1,17 @@
-#Argumentos 
-# $1 = nombre del archivo
-# $2 =numero del individuo
+#Argumentos $1 = nombre del archivo
 
 #1
 echo "Muestras para ID 3"
 cut -f 1 $1 | grep -c -w 3
 
-echo "Muestras para el ID 4"
+echo "Muestras para el ID 27"
 cut -f 1 $1 | grep -c -w 27
 
-
-
-
-
-
-
+#2y3
+echo "Numero de registros para cada ID "
 cut -f 1 $1 >1.csv
 tail -n +2 1.csv >2.csv
 uniq -c 2.csv
-#echo "Cantidad de registros para el identificador ingresado"
-#ls -l 3.csv| awk '{print$2}'
+echo "REGISTROS , ID"
 rm *.csv
 
