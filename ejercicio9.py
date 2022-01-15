42ea4fb96402550b7d991aac02f637409b696a83
manumos <manuela.moscoso@est.ikiam.edu.ec>
2022-01-15 11:48:20 -0500

    Moscoso Manuela ha resuelto el ejercicio 9

---
+++ ejercicio9.py
@@ -0,0 +1,24 @@
+dic_genoma= dict(
+    
+    Saccharomyces_cerevisiae= [120 , 6],
+    Trichomonas_vaginalis= [160 , 60],
+    Plasmodium_falciparum = [23 , 5],
+    Caenorhabditis_elegans= [955 , 18],
+    Drosophila_melanogaste= [170 , 14],
+    Arabidopsis_thaliana= [125 , 25],
+    Oryza_sativa= [470 , 51],
+    Gallus_gallus= [1000 , 20],
+    Canis_familiaris= [24000,19],
+    Mus_musculus= [25000 , 30],
+    Homo_sapiens= [29000 , 20],
+
+)
+
+
+especies =(dic_genoma.keys())
+
+print(especies)
+
+genomas =(dic_genoma.values())
+
+print(genomas)
