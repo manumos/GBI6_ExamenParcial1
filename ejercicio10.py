2d39f9afb37c4dd1c80d0d8bcfa9cde8295cc554
manumos <manuela.moscoso@est.ikiam.edu.ec>
2022-01-15 11:49:41 -0500

    Moscoso Manuela ha resuelto el ejercicio 10

---
+++ ejercicio10.py
@@ -0,0 +1,10 @@
+lista1= [8.6, 18.0, 19.4, 13.7, 11.1, 10.1, 10.3, 15.6, 14.9, 9.1]
+#print (lista1)
+lista1.append(["gbi6", "bio1", "ikiam"])
+lista1[-1][2]
+sum(lista1)/len(lista1)
+lista1[8:]
+lista1[5]
+sum(lista1)/len(lista1)
+round(sum(lista1)/len(lista1),3)
+lista1.append(["gbi6", "bio1", "ikiam"])
