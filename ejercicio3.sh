97da6161b84acc7dbe42ce488986d80339882679
manumos <manuela.moscoso@est.ikiam.edu.ec>
2022-01-15 11:28:15 -0500

    Moscoso Manuela ha resuelto el ejercicio 3

---
+++ ejercicio3.sh
@@ -0,0 +1,7 @@
+pwd
+mkdir test1
+touch test1/test1.txt
+echo "Hola GBI6"> test1/test2.txt
+echo "print("Hola GBI6")">test1/test1.py
+echo "El examen parcial de GBI6 es hoy" >>test1/test2.txt
+cat test1/test2.txt
